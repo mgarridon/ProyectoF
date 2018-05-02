616d64031cb44a0a700394cfb6d1a4dccd385975
jorge <jorge.saavedrac@utem.cl>
2018-05-02 19:47:51 -0300

    Validacion tabla cliente

--- ProyectoF2/app/models/cliente.rb
+++ ProyectoF2/app/models/cliente.rb
@@ -1,2 +1,6 @@
 class Cliente < ApplicationRecord
+  validates :nombre , presence: { message: 'Ingresar Nombre válido' }
+  validates :apellido , presence: { message: 'Ingresar Apellido válido' }
+  validates :telefono, numericality: true, length: { maximum: 9 }
+  validates :correo, uniqueness:{ message: 'Correo existente' }
 end
