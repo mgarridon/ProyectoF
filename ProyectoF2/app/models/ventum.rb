a989f1c09593974bc7e22ce6c18fd2df40c3fb23
jorge <jorge.saavedrac@utem.cl>
2018-05-02 20:15:51 -0300

    Validacion cliente y venta

--- ProyectoF2/app/models/ventum.rb
+++ ProyectoF2/app/models/ventum.rb
@@ -1,3 +1,8 @@
 class Ventum < ApplicationRecord
   belongs_to :cliente
+
+  validates :id_producto_venta, numericality: true
+  validates :cantidad_venta, numericality: true
+  validates :precio_venta, numericality: true
+  validates :fecha_venta, numericality: true
 end
