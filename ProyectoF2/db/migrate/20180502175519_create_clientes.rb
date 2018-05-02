fbed9e439ff4334b66afe911248b2e8cc3c8b640
jorge <jorge.saavedrac@utem.cl>
2018-05-02 14:56:43 -0300

    Creacion modelo CLiente

---
+++ ProyectoF2/db/migrate/20180502175519_create_clientes.rb
@@ -0,0 +1,12 @@
+class CreateClientes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :clientes do |t|
+      t.string :nombre
+      t.integer :telefono
+      t.string :direccion
+      t.string :correo
+
+      t.timestamps
+    end
+  end
+end
